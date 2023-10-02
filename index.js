13e3a73ec22730569e7840a19e1f3953915f68a0
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-01 20:53:32 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, `;
+welcomeMessage +='!';
+excitedWelcomeMessage += `${currentUser}`.toUpperCase();
+excitedWelcomeMessage += `!`;
+let shortGreeting = "Welcome, ";
+shortGreeting += currentUser[0];
+shortGreeting +="!";
